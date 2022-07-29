25b8c70b0bc61e4e2bc83c15bae56a0a06b736a0
Sergey Komarov <anaxronik@gmail.com>
2022-07-29 17:23:44 +0500

    add MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
-import type { AppProps } from "next/app";
+import "../styles/global.css";
 
+import Head from "next/head";
+
+import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+    </Component>
+  );
 }
 
 export default MyApp;
